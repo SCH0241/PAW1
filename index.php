7dc7d9d77891fcd88e5820b3f39e1a56d7ed08b0
SCH0241 <schustekdominik@gmail.com>
2016-10-05 10:26:05 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html!>
+<html dir="ltr" lang="">
+  <head>       
+    
+    <title>PAW1</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="robots" content="index, follow">
+	<meta name="revisit-after" content="7 days">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	
+	</head>  
+	
+<?php
+	echo "Ahoj Karle bla bla";
+	for($1=0;  $<10;  $i++;)  { 
+		echo  $i. "blablubla";
+		
+		}
+?>
+</body>
+</html>    
